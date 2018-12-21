f9d24705330ed86434a36b939e59672dbee5ad8a
Theodor Tolstoy <ttolstoy@ebsco.com>
2018-12-21 22:38:37 +0100

    refinement after feedback

--- chalmersDumpHoldingsadder.py
+++ chalmersDumpHoldingsadder.py
@@ -13,6 +13,8 @@ file_libris = sys.argv[3]
 t_recs = {}
 
 sigels = ['Za', 'Z', 'Zl', 'Enll']
+
+
 def code_to_sigel(code):
     if code.startswith('abib'):
         return 'Za'
@@ -44,13 +46,27 @@ def add_subjects(record, field, sigels):
 
 
 def in_035(recs, rec):
-    return ('035' in rec and 'a' in rec['035'] and rec['035']['a'].upper() in recs)
+    return ('035' in rec and
+            'a' in rec['035'] and
+            rec['035']['a'].upper() in recs)
 
 
-s = 0
+def write_rec(out_file, rec):
+    try:
+        mrc = rec.as_marc()
+        out_file.write(mrc)
+    except Exception as ee:
+        print("Något gick fel med att spara {}".format(rec['001']))
+        print(ee, flush=True)
+
+
+i = 0
 p = 0
 sh = 0
+s = 0
 adds = 0
+without_zsh = 0
+si_bib_ids = {}
 with open(file_in, 'rb') as fi:
     reader = MARCReader(fi, 'rb')
     for rec in reader:
@@ -60,7 +76,7 @@ with open(file_in, 'rb') as fi:
         sigels = set()
         if i % 10000 == 0:
             elapsed = i/(time.time() - start)
-            print("56{}\t{}\t{}".format(elapsed, i, file_in))
+            print("{}\t{}\t{}".format(elapsed, i, file_in), flush=True)
         if '907' in rec and 'c' in rec['907']:
             if rec['907']['c'] == 'p':
                 p += 1
@@ -74,10 +90,11 @@ with open(file_in, 'rb') as fi:
                     sigels.add(c)
                     adds += 1
             else:
-                print('else1')
+                print('else1', flush=True)
+            si_bib_ids[iD] = rec['907']['a']
         else:
-            print("else")
-            print("{}\t{}".format(iD, list(sigels)))
+            print("else", flush=True)
+            print("{}\t{}".format(iD, list(sigels)), flush=True)
         for sigel in sigels:
             t_recs[iD].append(add_holding(rec, sigel))
         if '698' in rec:
@@ -85,51 +102,61 @@ with open(file_in, 'rb') as fi:
             for fs in rec.get_fields('698'):
                 add_subjects(rec, fs, sigels)
         else:
-            print('{} utan lokala ämnesord'.format(iD))
+            without_zsh += 1
     elapsed = i/(time.time() - start)
-    print("85{}\t{}\t{}".format(elapsed, i, len(t_recs)))
-missing = 0
+    print("85{}\t{}\t{}".format(elapsed, i, len(t_recs)), flush=True)
+missing = set()
 saves = 0
 match_001 = 0
 match_035 = 0
-found_ids = []
+found_ids = set()
 with open(file_out, 'wb+') as fo:
     with open(file_libris, 'rb') as fl:
         reader = MARCReader(fl, 'rb')
         j = 0
         smart = time.time()
         for l_rec in reader:
-            if '698' in l_rec and '5' in l_rec['698'] and l_rec['698']['5'] in sigels:
-                print("698")
+            has_cth_698 = ('698' in l_rec and
+                           '5' in l_rec['698'] and
+                           l_rec['698']['5'] in sigels)
+            if has_cth_698:
+                print("698", flush=True)
             iD = l_rec['001'].data.upper()
             j += 1
             if j % 10000 == 0:
                 elapsed = j/(time.time() - smart)
-                print("94{}\t{}\t{}".format(elapsed, j, file_in))
+                print("{}\t{}\t{}".format(elapsed, j, file_in), flush=True)
             l_rec.remove_fields('852')
             l_rec.remove_fields('866')
             if iD in t_recs:
                 match_001 += 1
                 for t_field in t_recs[iD]:
                     l_rec.add_field(t_field)
-                fo.write(l_rec.as_marc())
+                write_rec(fo, l_rec)
                 saves += 1
-                found_ids.append(iD)
+                found_ids.add(iD)
             elif in_035(t_recs, l_rec):
                 match_035 += 1
                 old_id = l_rec['035']['a'].upper()
                 for t_field in t_recs[old_id]:
                     l_rec.add_field(t_field)
-                fo.write(l_rec.as_marc())
+                write_rec(fo, l_rec)
                 saves += 1
-                found_ids.append(old_id)
+                found_ids.add(old_id)
             else:
-                missing += 1
-for mid in t_recs.keys():
-    if mid not in found_ids:
-        print("Ohittad:\t{}".format(mid))
+                missing.add(iD)
+print("All written. The following ids where not fount", flush=True)
 
-print("Missing from Sierra:\t\t{}".format(missing))
+set_sierra = set(t_recs.keys())
+unmatched = set_sierra - found_ids
+for nolletta in unmatched:
+    print(si_bib_ids[nolletta])
+print("001:or i sierra utan match i 001/035a i Libris", flush=True)
+print(len(unmatched))
+# print(list(unmatched), flush=True)
+
+print("Missing from Sierra:\t\t{}".format(len(missing)))
+# print(list(missing))
 print("S in 907$c:\t\t{}".format(s))
 print("P in 907$c:\t\t{}".format(p))
 print("Added subject headings:\t\t{}".format(sh))
@@ -139,3 +166,4 @@ print("Records from Sierra:\t\t{}".format(i))
 print("Records from Libris:\t\t{}".format(j))
 print("Sierras 001 i Libris 001:\t\t{}".format(match_001))
 print("Sierras 001 i Libris 035:\t\t{}".format(match_035))
+print("Utan lokala ämnesord i 698:\t\t{}".format(without_zsh))
